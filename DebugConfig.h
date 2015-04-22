174ab6983c0305e5edff23c64882fe6864d07602
Helge Reelfs <helge@iotduino.de>
2015-04-22 23:10:10 +0200

    Revert "added debug configuraiton header"
    This reverts commit b77eda89454a459911906510fa4faf9229b70ae4.

---
+++ DebugConfig.h
@@ -0,0 +1,18 @@
+/*
+ * DebugConfig.h
+ *
+ * Created: 08.04.2015 20:46:51
+ *  Author: helge
+ */ 
+
+
+#ifndef DEBUGCONFIG_H_
+#define DEBUGCONFIG_H_
+
+
+//#define DEBUG_NETWORK_ENABLE
+#define DEBUG_HANDLER_ENABLE
+#define DEBUG_HARDWARE_ENABLE
+
+
+#endif /* DEBUGCONFIG_H_ */
