6451e98d8bbc5074b62aea104d9b4c8725cf590d
Helge Reelfs <helge@iotduino.de>
2015-08-30 17:12:47 +0200

    moved sdcard implementation into source file

--- sdcard/SDcard.h
+++ sdcard/SDcard.h
@@ -86,31 +86,14 @@ class SDcard {
 		 * delete all subscription infos
 		 * @return success
 		 */
-		boolean deleteSubscriptionInfo() {
-			boolean success = true;
-			for(uint8_t i = 0; i < SD_DISCOVERY_NUM_SUBSCRIPTIONS_PER_NODE; i++) {
-				success &= deleteSubscriptionInfo(i);
-			}
-
-			return success;
-		}
+		boolean deleteSubscriptionInfo();
 
 		/**
 		 * delete all subscriptions for a node
 		 * @param nodeId
 		 * @return success
 		 */
-		boolean deleteSubscriptionInfo(uint8_t nodeId) {
-			if(nodeId >= SD_DISCOVERY_NUM_NODES)
-				return false;
-
-			boolean success = true;
-			for(uint8_t i = 0; i < SD_DISCOVERY_NUM_SUBSCRIPTIONS_PER_NODE; i++) {
-				success &= deleteSubscriptionInfo(nodeId, i);
-			}
-
-			return success;
-		}
+		boolean deleteSubscriptionInfo(uint8_t nodeId);
 
 		/**
 		 * delete subscription for a node
@@ -118,16 +101,7 @@ class SDcard {
 		 * @param index
 		 * @return success
 		 */
-		boolean deleteSubscriptionInfo(uint8_t nodeId, uint8_t index) {
-			if(nodeId >= SD_DISCOVERY_NUM_NODES || index >= SD_DISCOVERY_NUM_INFOS_PER_NODE)
-				return false;
-
-			uint8_t tmp[sizeof(SD_subscriptionInfoTableEntry_t)];
-			memset(tmp, 0, sizeof(tmp));
-
-			uint32_t pos = getSubscriptionInfosAddressForNodeById(nodeId, index);
-			return discoveryWriteToSD(pos, tmp, sizeof(SD_subscriptionInfoTableEntry_t));
-		}
+		boolean deleteSubscriptionInfo(uint8_t nodeId, uint8_t index);
 
 		/**
 		 * save subscriptions
@@ -136,22 +110,14 @@ class SDcard {
 		 * @param info
 		 * @return success
 		 */
-		boolean saveSubscriptionInfo(uint8_t nodeId, uint8_t index, SD_subscriptionInfoTableEntry_t* info) {
-			if(nodeId >= SD_DISCOVERY_NUM_NODES || index >= SD_DISCOVERY_NUM_INFOS_PER_NODE)
-				return false;
-
-			uint32_t pos = getSubscriptionInfosAddressForNodeById(nodeId, index);
-			return discoveryWriteToSD(pos, (uint8_t*) info, sizeof(SD_subscriptionInfoTableEntry_t));
-		}
+		boolean saveSubscriptionInfo(uint8_t nodeId, uint8_t index, SD_subscriptionInfoTableEntry_t* info);
 
 		/**
 		 * @param nodeId
 		 * @param index
 		 * @return address
 		 */
-		inline uint32_t getSubscriptionInfosAddressForNodeById(uint8_t nodeId, uint8_t index) {
-			return subscriptionInfoStart + (nodeId * SD_DISCOVERY_NUM_SUBSCRIPTIONS_PER_NODE * index) * sizeof(SD_subscriptionInfoTableEntry_t);
-		}
+		uint32_t getSubscriptionInfosAddressForNodeById(uint8_t nodeId, uint8_t index);
 
 		/**
 		 * save set of subscription infos
@@ -160,48 +126,20 @@ class SDcard {
 		 * @param numInfos
 		 * @return success
 		 */
-		boolean saveSubscriptionsInfos(uint8_t nodeId, SD_subscriptionInfoTableEntry_t* info, uint8_t numInfos) {
-			if(numInfos > SD_DISCOVERY_NUM_INFOS_PER_NODE)
-				return false;
-
-			//prepare data
-			SD_subscriptionInfoTableEntry_t data[SD_DISCOVERY_NUM_SUBSCRIPTIONS_PER_NODE];
-			memset(&data[numInfos], 0, sizeof(data) - numInfos * sizeof(SD_subscriptionInfoTableEntry_t));
-			memcpy(data, info, numInfos * sizeof(SD_subscriptionInfoTableEntry_t));
-
-			//write to sd
-			uint32_t pos = getSubscriptionsInfosAddressForNode(nodeId);
-			uint16_t len = sizeof(data);
-			return discoveryWriteToSD(pos, (uint8_t*) data, len);
-		}
+		boolean saveSubscriptionsInfos(uint8_t nodeId, SD_subscriptionInfoTableEntry_t* info, uint8_t numInfos);
 
 		/**
 		 * delete all discovery infos
 		 * @return success
 		 */
-		boolean deleteDiscoveryInfosAll() {
-			boolean success = true;
-			for(uint8_t i = 0; i < SD_DISCOVERY_NUM_NODES; i++) {
-				success &= deleteDiscoveryInfos(i);
-			}
-			return success;
-		}
+		boolean deleteDiscoveryInfosAll();
 
 		/**
 		 * delete discovery infos for a node
 		 * @param nodeId
 		 * @return success
 		 */
-		boolean deleteDiscoveryInfos(uint8_t nodeId) {
-			if(nodeId >= SD_DISCOVERY_NUM_NODES)
-				return false;
-			uint8_t tmp[SD_DISCOVERY_NUM_INFOS_PER_NODE * sizeof(SD_nodeDiscoveryInfoTableEntry_t)];
-			memset(tmp, 0, sizeof(tmp));
-
-			//write to sd
-			uint32_t pos = getDiscoveryInfosAddressForNode(nodeId);
-			return discoveryWriteToSD(pos, tmp, sizeof(tmp));
-		}
+		boolean deleteDiscoveryInfos(uint8_t nodeId);
 
 		/**
 		 * save set of discovery infos
@@ -211,13 +149,7 @@ class SDcard {
 		 * @param startIndex
 		 * @return success
 		 */
-		boolean saveDiscoveryInfos(uint8_t nodeId, SD_nodeDiscoveryInfoTableEntry_t* info, uint8_t numInfos, uint8_t startIndex) {
-			if(nodeId >= SD_DISCOVERY_NUM_NODES || numInfos > SD_DISCOVERY_NUM_INFOS_PER_NODE - startIndex || info == NULL)
-				return false;
-			uint32_t pos = getDiscoveryInfosAddressForNode(nodeId) + startIndex * sizeof(SD_nodeDiscoveryInfoTableEntry_t);
-			uint16_t len = numInfos * sizeof(SD_nodeDiscoveryInfoTableEntry_t);
-			return discoveryWriteToSD(pos, (uint8_t*) info, len);
-		}
+		boolean saveDiscoveryInfos(uint8_t nodeId, SD_nodeDiscoveryInfoTableEntry_t* info, uint8_t numInfos, uint8_t startIndex);
 
 		/**
 		 * save set of discovery infos
@@ -226,20 +158,7 @@ class SDcard {
 		 * @param numInfos
 		 * @return success
 		 */
-		boolean saveDiscoveryInfos(uint8_t nodeId, SD_nodeDiscoveryInfoTableEntry_t* info, uint8_t numInfos) {
-			if(nodeId >= SD_DISCOVERY_NUM_NODES || numInfos > SD_DISCOVERY_NUM_INFOS_PER_NODE || info == NULL)
-				return false;
-
-			//prepare data
-			SD_nodeDiscoveryInfoTableEntry_t data[SD_DISCOVERY_NUM_INFOS_PER_NODE];
-			memset(&data[numInfos], 0, (SD_DISCOVERY_NUM_INFOS_PER_NODE - numInfos) * sizeof(SD_nodeDiscoveryInfoTableEntry_t));
-			memcpy(data, info, numInfos * sizeof(SD_nodeDiscoveryInfoTableEntry_t));
-
-			//write to sd
-			uint32_t pos = getDiscoveryInfosAddressForNode(nodeId);
-			uint16_t len = sizeof(data);
-			return discoveryWriteToSD(pos, (uint8_t*) data, len);
-		}
+		boolean saveDiscoveryInfos(uint8_t nodeId, SD_nodeDiscoveryInfoTableEntry_t* info, uint8_t numInfos);
 
 		/**
 		 * write data to discovery info file
@@ -248,25 +167,7 @@ class SDcard {
 		 * @param bytes
 		 * @return success
 		 */
-		boolean discoveryWriteToSD(uint32_t pos, void* buf, size_t len) {
-			if(seekDiscovery(pos)) {
-				uint32_t bytes = myFileDiscovery.write((uint8_t*) buf, len);
-				myFileDiscovery.flush();
-
-				#ifdef DEBUG_SD_ENABLE
-					Serial.print(millis());
-					Serial.print(F(": SDcard::discoveryWriteToSD() pos="));
-					Serial.print(pos);
-					Serial.print(F(" len="));
-					Serial.print(len);
-					Serial.print(F(" written="));
-					Serial.println(bytes);
-				#endif
-
-				return bytes;
-			}
-			return false;
-		}
+		boolean discoveryWriteToSD(uint32_t pos, void* buf, size_t len);
 
 		/**
 		 * save node info
@@ -274,13 +175,7 @@ class SDcard {
 		 * @param info
 		 * @return success
 		 */
-		boolean saveNodeInfo(uint8_t nodeId, SD_nodeInfoTableEntry_t* info) {
-			if(nodeId >= SD_DISCOVERY_NUM_NODES || info == NULL)
-				return false;
-			uint32_t pos = nodeId * sizeof(SD_nodeInfoTableEntry_t);
-			uint16_t len = sizeof(SD_nodeInfoTableEntry_t);
-			return discoveryWriteToSD(pos, (uint8_t*) info, len);
-		}
+		boolean saveNodeInfo(uint8_t nodeId, SD_nodeInfoTableEntry_t* info);
 
 		/**
 		 * get subscription infos for node
@@ -289,21 +184,13 @@ class SDcard {
 		 * @param numEntries of buffer
 		 * @return success
 		 */
-		boolean getSubscriptionInfosForNode(uint8_t nodeId, SD_subscriptionInfoTableEntry_t* buf, uint8_t numEntries) {
-			if(nodeId >= SD_DISCOVERY_NUM_NODES || numEntries < SD_DISCOVERY_NUM_SUBSCRIPTIONS_PER_NODE || buf == NULL)
-				return false;
-			//read
-			seekDiscovery(getSubscriptionsInfosAddressForNode(nodeId));
-			return myFileDiscovery.readBytes((uint8_t*) buf, numEntries * SD_DISCOVERY_NUM_SUBSCRIPTIONS_PER_NODE * sizeof(SD_subscriptionInfoTableEntry_t));
-		}
+		boolean getSubscriptionInfosForNode(uint8_t nodeId, SD_subscriptionInfoTableEntry_t* buf, uint8_t numEntries);
 
 		/**
 		 * @param nodeId
 		 * @return address
 		 */
-		inline uint32_t getSubscriptionsInfosAddressForNode(uint8_t nodeId) {
-			return subscriptionInfoStart + nodeId * sizeof(SD_subscriptionInfoTableEntry_t);
-		}
+		uint32_t getSubscriptionsInfosAddressForNode(uint8_t nodeId);
 
 		/**
 		 * get discovery infos for node
@@ -312,19 +199,9 @@ class SDcard {
 		 * @param numEntries of buffer
 		 * @return success
 		 */
-		boolean getDiscoveryInfosForNode(uint8_t nodeId, SD_nodeDiscoveryInfoTableEntry_t* buf, uint8_t numEntries) {
-			if(nodeId >= SD_DISCOVERY_NUM_NODES || numEntries < SD_DISCOVERY_NUM_INFOS_PER_NODE || buf == NULL)
-				return false;
-			//read
-			if(!seekDiscovery(getDiscoveryInfosAddressForNode(nodeId)))
-				return false;
+		boolean getDiscoveryInfosForNode(uint8_t nodeId, SD_nodeDiscoveryInfoTableEntry_t* buf, uint8_t numEntries);
 
-			return myFileDiscovery.readBytes((uint8_t*) buf, SD_DISCOVERY_NUM_INFOS_PER_NODE * sizeof(SD_nodeDiscoveryInfoTableEntry_t));
-		}
-
-		inline uint32_t getDiscoveryInfosAddressForNode(uint8_t nodeId) {
-			return discoveryInfoStart + nodeId * SD_DISCOVERY_NUM_INFOS_PER_NODE * sizeof(SD_nodeDiscoveryInfoTableEntry_t);
-		}
+		uint32_t getDiscoveryInfosAddressForNode(uint8_t nodeId);
 
 		/**
 		 * get node info
@@ -332,24 +209,7 @@ class SDcard {
 		 * @param buf data object
 		 * @return success
 		 */
-		boolean getDiscoveryNodeInfo(uint8_t nodeId, SD_nodeInfoTableEntry_t* buf) {
-			if(nodeId >= SD_DISCOVERY_NUM_NODES || buf == NULL)
-				return false;
-			//read
-			if(!seekDiscovery(getNodeDiscoveryInfoAddress(nodeId))) {
-				#ifdef DEBUG_SD_ENABLE
-					Serial.print(millis());
-					Serial.print(F(" getDiscoveryNodeInfo() seek for node="));
-					Serial.print(nodeId);
-					Serial.println(F(" failed."));
-				#endif
-				return false;
-			}
-
-			boolean ret = false;
-			ret = myFileDiscovery.readBytes((uint8_t*) buf, sizeof(SD_nodeInfoTableEntry_t));
-			return ret;
-		}
+		boolean getDiscoveryNodeInfo(uint8_t nodeId, SD_nodeInfoTableEntry_t* buf);
 
 		/** empty constructor*/
 		SDcard() {};
@@ -370,48 +230,26 @@ class SDcard {
 		 */
 		uint8_t getNodeInfoString(uint8_t nodeId, uint8_t* buf, uint8_t bufSize);
 
-		inline uint32_t getNodeDiscoveryInfoAddress(uint8_t nodeId) {
-			return nodeId * sizeof(SD_nodeInfoTableEntry_t);
-		}
+		uint32_t getNodeDiscoveryInfoAddress(uint8_t nodeId);
 
 		/**
 		 * save node info. adds terminating character.
 		 */
-		boolean saveNodeInfoString(uint8_t nodeId, uint8_t* buf, uint8_t bufSize) {
-			if(bufSize < NODE_INFO_SIZE)
-				return false;
-			buf[NODE_INFO_SIZE-1] = '\0';
-			uint32_t pos = nodeId * NODE_INFO_SIZE;
-			if(!myFileInfo.seek(pos)) {
-				return false;
-			}
-			boolean success = myFileInfo.write(buf, NODE_INFO_SIZE);
-			myFileInfo.flush();
-			return success;
-		};
+		boolean saveNodeInfoString(uint8_t nodeId, uint8_t* buf, uint8_t bufSize);;
 
 
 		/**
 		 * @param pos
 		 * @success
 		 */
-		boolean seekDiscovery(uint32_t pos) {
-			if(!myFileDiscovery)
-				return false;
-			return myFileDiscovery.seek(pos);
-		}
+		boolean seekDiscovery(uint32_t pos);
 
 		/**
 		 * @param pos
 		 * @success
 		 */
-		boolean seekInfo(uint32_t pos) {
-			if(!myFileInfo)
-				return false;
-			return myFileInfo.seek(pos);
-		}
+		boolean seekInfo(uint32_t pos);
 
-	//protected:
 		/**
 		 * internal method for opening a file
 		 * @param filName
